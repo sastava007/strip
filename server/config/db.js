7c61f40308438eaaa9f9e317a145cfcf35283da1
BALAJIRAO676 <balu14012001@gmail.com>
2019-09-29 23:23:18 +0530

    safe-made

--- server/config/db.js
+++ server/config/db.js
@@ -3,8 +3,8 @@ const config = require('config');
 
 const connectDB = async () => {
     try {
-
-        await mongoose.connect(config.get('mongoURI'), { useNewUrlParser: true });
+        
+        await mongoose.connect(`mongodb+srv://sastava007:${process.env.password}@strip-pwqe1.gcp.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true });
         console.log('Connected to Database');
 
     } catch (err) {
